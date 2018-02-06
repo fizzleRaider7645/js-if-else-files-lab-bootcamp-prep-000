c2a66226ae74b55dc95dc3380231ff3e0a3fecdf
Douglas Uretsky <douglasuretsky@gmail.com>
2018-02-06 22:51:11 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -6,7 +6,7 @@ if (index.name === "Susan") {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
-
+https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/programming-js-in-the-ide/practicing-conditional-statements#
 
 if (index.height === 70) {
     console.log("The height is correct")
